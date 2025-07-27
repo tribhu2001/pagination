2f33e75a5a398f6c9608c482fdb943f24d906c60
tribhu2001 <79187488+tribhu2001@users.noreply.github.com>
2025-07-27 23:55:10 +0530

    New committed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [products,setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const fetchProduct = async()=>{
+    const res = await fetch(`https://dummyjson.com/products?limit=10&skip=${page*10-10}`);
+    const data = await res.json();
+    setProducts(data.products);
+    setTotalPages(Math.ceil(data.total / 10));
+  }
+
+  const pageHandler = (pageNumber) => {
+    if(pageNumber < 1 || pageNumber > totalPages) return;
+    setPage(pageNumber);
+  }
+
+  useEffect(()=>{
+    fetchProduct();
+  },[page]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='products'>
+      {products.map((prod)=>{
+          return(
+            <span className='products__single' key={prod.id}>
+              <img src={prod.thumbnail}/>
+              <span>{prod.title}</span>
+            </span>
+          )
+      })}
     </div>
+    <div className='pagination'>
+        <span onClick={()=>pageHandler(page-1)}>◀</span>
+        {[...Array(totalPages)].map((_, index) => (
+          <span key={index} onClick={()=>pageHandler(index+1)} className={index+1 === page ? 'active' : ''}>
+            {index+1}
+          </span>)
+        )}
+        <span onClick={()=>pageHandler(page+1)}>▶</span>
+    </div>
+  </>
   );
 }
 
